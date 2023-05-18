018433405d29a344be449373cf6943294ac69928
juliocesarmarquez <juliocesarmarquez88@gmail.com>
2023-05-18 15:48:15 -0300

    initial

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,18 @@
 import Layout from './components/Layout/Layout'
 import styles from './page.module.css'
 
-export default function Home() {
+export default function Home({countries}) {
+  console.log(countries);
   return <Layout>main</Layout>;
 }
+
+export const getStaticProps = async () => {
+  const res = await fetch("https://restcountries.com/rest/v2/all");
+  const countries = await res.json();
+
+  return {
+    props: {
+      countries,
+    },
+  };
+};
